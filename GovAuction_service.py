62b89423d41cfb35e5d7de671c54e48926f63898
Ole <login15911591@gmail.com>
2020-02-26 12:52:21 +0200

    lots info view

--- GovAuction_service.py
+++ GovAuction_service.py
@@ -186,16 +186,20 @@ def adapt_view_tender_data(value, field_name):
 
 
 def adapt_view_lot_data(value, field_name):
+    # if 'value.amount' in field_name:
+    #     value = float("".join(value.split(' ')[:-4]))
     if 'value.amount' in field_name:
-        value = float("".join(value.split(' ')[:-4]))
+        value = float(value.replace(' ', ''))
     elif 'minimalStep.currency' in field_name:
         value = value.split(' ')[-1]
     elif 'currency' in field_name:
         value = value.split(' ')[-4]
     elif 'valueAddedTaxIncluded' in field_name:
         value = ' '.join(value.split(' ')[-3:]).strip()
+    # elif 'minimalStep.amount' in field_name:
+    #     value = float("".join(value.split(' ')[:-1]))
     elif 'minimalStep.amount' in field_name:
-        value = float("".join(value.split(' ')[:-1]))
+        value = float(value.replace(' ', ''))
     elif 'Date' in field_name:
         value = convert_time(value)
     return convert_string_from_dict_GovAuction(value)
